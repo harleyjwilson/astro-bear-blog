73201f4759f39dd03140ad8b3f1f88d26b6ec4fe
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2024-05-30 20:56:40 +0800

    Add blog functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import bearBlogTheme from './src/bear-blog-theme.json';
 import { SITE_URL } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+  markdown: {
+    shikiConfig: { theme: bearBlogTheme },
+  },
 });
