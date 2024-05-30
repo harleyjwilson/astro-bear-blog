63b4404d3638bdce466ff407e5f7570027f927c6
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2024-05-30 17:46:54 +0800

    Initial Bear Blog commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Astro Bear Blog Theme
---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import { SITE_URL } from './src/consts';
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE_URL,
+});
